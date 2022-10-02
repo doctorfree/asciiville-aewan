5615cd2ea295d5e8e23b54cbeb78d2efbbeb899b
Ronald Record <github@ronrecord.com>
2022-10-02 09:07:28 -0700

    Packaging

--- pkg/mkdeb.sh
+++ pkg/mkdeb.sh
@@ -47,9 +47,9 @@ OUT_DIR="dist/${PKG_NAME}_${PKG_VER}"
 cd "${SRC}/${SRC_NAME}"
 
 # Build aewan
-if [ -x scripts/build-aewan.sh ]
+if [ -x pkg/build-aewan.sh ]
 then
-  scripts/build-aewan.sh
+  pkg/build-aewan.sh
 else
   python3 waf configure --prefix=/usr \
                         --build-static \
@@ -69,26 +69,20 @@ chmod 755 ${OUT_DIR} ${OUT_DIR}/DEBIAN
 
 echo "Package: ${PKG}
 Version: ${PKG_VER}-${PKG_REL}
-Section: sound
+Section: misc
 Priority: optional
 Architecture: ${ARCH}
-Depends: libtag1v5 (>= 1.11), python3-dev, libchromaprint-dev, libeigen3-dev, libfftw3-dev, libsamplerate0, libyaml-dev, libavformat58 (>= 7:4.1), libavfilter7 (>= 7:4.0), libswresample3 (>= 7:4.0), libavcodec58 (>= 7:4.2), libswscale5 (>= 7:4.0), libavdevice58 (>= 7:4.0), libavutil56 (>= 7:4.0)
+Depends: libncursesw6 (>= 6), zlib1g
 Maintainer: ${DEBFULLNAME} <${DEBEMAIL}>
-Installed-Size: 3000
 Build-Depends: debhelper (>= 11)
 Homepage: https://github.com/doctorfree/asciiville-aewan
-Description: Essentia audio analysis tool -- feature extractors
- Essentia is an audio analysis tool. It provides a reusable collection
- of algorithms and descriptors mainly used to extract features from
- audio files. It is written in C++, but also has Python bindings,
- meaning you can control it from within Python, and write scripts that
- use Essentia features (just as you would in Matlab).
- .
- This package contains command-line feature extractors based on Essentia." > ${OUT_DIR}/DEBIAN/control
+Description: Aewan is a curses-based program for the creation and editing of ascii art" > ${OUT_DIR}/DEBIAN/control
 
 chmod 644 ${OUT_DIR}/DEBIAN/control
 
 for dir in "${DESTDIR}" "${DESTDIR}/share" "${DESTDIR}/share/doc" \
+           "${DESTDIR}/share/man" "${DESTDIR}/share/man/man1" \
+           "${DESTDIR}/share/man/man5" \
            "${DESTDIR}/share/doc/${PKG}" "${DESTDIR}/share/${PKG}"
 do
     [ -d ${OUT_DIR}/${dir} ] || ${SUDO} mkdir ${OUT_DIR}/${dir}
@@ -100,31 +94,26 @@ do
     [ -d ${OUT_DIR}/${DESTDIR}/${dir} ] && ${SUDO} rm -rf ${OUT_DIR}/${DESTDIR}/${dir}
 done
 
-#${SUDO} cp aewan/build/src/examples/aewan_streaming_extractor_music \
-#           ${OUT_DIR}/${DESTDIR}/bin
-#${SUDO} cp aewan/build/src/examples/aewan_streaming_extractor_music_svm \
-#           ${OUT_DIR}/${DESTDIR}/bin
 # Install aewan
-if [ -x scripts/build-aewan.sh ]
+if [ -x pkg/build-aewan.sh ]
 then
-  ${SUDO} scripts/build-aewan.sh -i -d "${SRC}/${SRC_NAME}/${OUT_DIR}"
+  ${SUDO} pkg/build-aewan.sh -i -d "${SRC}/${SRC_NAME}/${OUT_DIR}"
 else
-  ${SUDO} python3 waf install --destdir="${SRC}/${SRC_NAME}/${OUT_DIR}"
+  ${SUDO} cp aewan aecat aemakeflic ${OUT_DIR}/${DESTDIR}/bin
+  ${SUDO} cp man/man1/*.1 ${OUT_DIR}/${DESTDIR}/share/man/man1
+  ${SUDO} cp man/man5/*.5 ${OUT_DIR}/${DESTDIR}/share/man/man5
 fi
 
-${SUDO} cp ACKNOWLEDGEMENTS ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
-${SUDO} cp AUTHORS ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
-${SUDO} cp Changelog ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
-${SUDO} cp COPYING.txt ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
+${SUDO} cp aewan-README ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
+${SUDO} cp copyright ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
+${SUDO} cp CHANGELOG ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
+${SUDO} cp COPYING ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
 ${SUDO} cp README.md ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
-${SUDO} cp "Essentia Licensing.txt" ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
-${SUDO} cp FAQ.md ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
+${SUDO} cp LICENSE ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
+${SUDO} cp TODO ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
 ${SUDO} cp VERSION ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
-${SUDO} cp MPPVERSION ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}
 ${SUDO} pandoc -f gfm README.md | ${SUDO} tee ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}/README.html > /dev/null
-${SUDO} gzip -9 ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}/Changelog
-
-${SUDO} cp -a share/svm_models ${OUT_DIR}/${DESTDIR}/share/${PKG}/svm_models
+${SUDO} gzip -9 ${OUT_DIR}/${DESTDIR}/share/doc/${PKG}/CHANGELOG
 
 ${SUDO} chmod 755 ${OUT_DIR}/${DESTDIR}/bin/* ${OUT_DIR}/${DESTDIR}/bin
 ${SUDO} chown -R root:root ${OUT_DIR}/${DESTDIR}
